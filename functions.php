c138464a0768e316f77ed4b6ba23c5a0bec09c73
C. Hamilton <cidney@cidney.org>
2015-04-01 14:05:27 -0400

    Update ISC base theme to be reusable on additional websites.

--- functions.php
+++ functions.php
@@ -5,8 +5,6 @@ add_action( 'wp_enqueue_scripts', 'isc_styles' );
 function isc_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 
-    wp_enqueue_style( 'serif-type', get_template_directory_uri().'/serif-type.css' );
-
     wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
 
